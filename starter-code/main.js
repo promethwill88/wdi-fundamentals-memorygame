6dcea83b0a3ed9d902157540e13ae4c59a5c7198
Will Fong <willfong@Wills-MacBook-Pro.local>
2017-03-21 18:35:55 -0700

    created function createCards

--- starter-code/main.js
+++ starter-code/main.js
@@ -5,9 +5,25 @@ var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
 
+/*
 if(cardTwo === cardFour){
 	alert("You found a match!");
 }
 else {
 	alert("Sorry, try again.");
 }
+*/
+
+//Create function
+var createCards = function(){
+//Get element with ID 'game-board'. Assign to var gameBoard
+	var gameBoard = document.getElementById('game-board');
+//Loop 4 times: create div with class name 'card', then append to gameBoard
+	for(var i = 0; i <= 3; i++){
+		var card = document.createElement('div');
+		card.className = 'card';
+		gameBoard.appendChild(card);
+	}
+};
+//Execute function
+createCards();
