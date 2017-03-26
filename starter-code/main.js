880045d22107e222a33c3e5c8a610caa6a5d1878
Will Fong <willfong@Wills-MacBook-Pro.local>
2017-03-26 12:33:12 -0700

    finished game logic - full working game

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,29 +1,70 @@
-//cardOne & cardTwo are queens
-var cardOne = "queen";
-var cardTwo = "queen";
-//cardThree & cardFour are kings
-var cardThree = "king";
-var cardFour = "king";
+// array containing all cards in game / can edit and change order of array
+var cards = ['queen', 'queen', 'king', 'king'];
+// cards that are in play, 2 at a time
+var cardsInPlay = [];
 
-/*
-if(cardTwo === cardFour){
-	alert("You found a match!");
-}
-else {
-	alert("Sorry, try again.");
-}
-*/
+//get element with ID 'game-board'. Assign to gameBoard
+var gameBoard = document.getElementById('game-board');
 
-//Create function
-var createCards = function(){
-//Get element with ID 'game-board'. Assign to var gameBoard
-	var gameBoard = document.getElementById('game-board');
-//Loop 4 times: create div with class name 'card', then append to gameBoard
-	for(var i = 0; i <= 3; i++){
-		var card = document.createElement('div');
-		card.className = 'card';
-		gameBoard.appendChild(card);
+function createBoard(){
+//loop cards.length array, creates div with class name 'card', then append to gameBoard
+	for(var i = 0; i < cards.length; i++){
+		var cardElement = document.createElement('div');
+		cardElement.className = 'card';
+		cardElement.setAttribute('data-card', cards[i]);
+		cardElement.addEventListener('click', isTwoCards);
+		gameBoard.appendChild(cardElement);
 	}
-};
-//Execute function
-createCards();
+}
+
+// checks to see if 2 cards are in play
+function isTwoCards(){
+	//add card to array of cards being viewed
+	cardsInPlay.push(this.getAttribute('data-card'));
+	//show card face image
+	if(this.getAttribute('data-card') === 'king'){
+		this.innerHTML = '<img src="king-159620_640.png" alt="King of Diamonds"/>';
+	}
+	else{
+		this.innerHTML = '<img src="diamonds-884199_640.png" alt="Queen of Diamonds"/>';
+	}
+	//check if 2 cards in play for match
+	if(cardsInPlay.length === 2){
+		//pass CardsInPlay to isMatch function
+		isMatch(cardsInPlay);
+		//clear cards in cardsInPlay Array for next try
+		cardsInPlay = [];
+	}
+}
+
+
+//alert pop up based on match or not
+function isMatch(){
+	// winning message alert
+	if(cardsInPlay[0] === cardsInPlay[1]){
+		alert("You found a match!");
+	}
+	else {
+		alert("Sorry, try again.");
+	}
+}
+
+//executes createBoard / start play
+createBoard();
+
+
+//Not working, resetting the board to first play tied to <button>
+function resetBoard(){
+	for(var i = 0; i < cards.length; i++){
+	cardElement.removeAttribute('data-card', cards[i]);
+	}
+}
+
+var resetButton = document.getElementById('reset');
+resetButton.addEventListener('click', resetBoard);
+
+
+
+
+
+
